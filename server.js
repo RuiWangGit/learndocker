2b1cfdf730b2d9beb737b874b70ebd915d5de008
Rui Wang <wangr3@gmail.com>
2016-01-29 23:04:52 -0800

    to docker

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ var restify = require('restify');
 var server = restify.createServer();
 
 server.get('/hello/:name', function(req, res, next){
-				res.send('hello '+ req.params.name);
-				next();
+  res.send('hello '+ req.params.name);
+	next();
 });
 
 
 
 
 server.listen(9090, function() {
-				console.log('%s listening at %s', server.name, server.url);
+	console.log('%s listening at %s', server.name, server.url);
+
+
 });
 
 
